c5ca588f88b24d50b01a77522d5e3d82a51fd578
MidnightNyx <61126532+MidnightNyx@users.noreply.github.com>
2020-02-16 15:49:43 -0800

    Add files via upload

---
+++ app.js
@@ -0,0 +1,28 @@
+const express = require('express')
+const path = require('path')
+const app = express()
+const APP_PORT = 5555
+
+const server = app.listen(APP_PORT, () => {
+  console.log(`App running on port ${APP_PORT}`)
+})
+const io = require('socket.io').listen(server)
+// This is config for render view in `views` folder
+// and use pug as template engine
+app.set('views', path.join(__dirname, 'views'))
+app.set('view engine', 'pug')
+
+app.use(express.static('public'))
+
+
+app.get('/', (req, res) => {
+  res.render('index')
+})
+
+io.on('connection', (socket) => {
+  console.log()
+  socket.on('chatter', (message) => {
+    console.log('message : ', message)
+    io.emit('chatter', message)
+  })
+})
